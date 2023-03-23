94242f74247a7d969c0f391c4d7767c7d1ece997
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-03-23 23:56:38 +0630

    feat: add control flow

---
+++ 02_fundamental/control_flow_with_if/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let a = 99;
+    if a > 200 {
+        println!("a is greater than 200")
+    }
+    else if a > 99 {
+        println!("a is greater than 99")
+    } else {
+        println!("a is less than 99")
+    }
+}
