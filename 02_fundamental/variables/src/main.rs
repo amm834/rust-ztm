2b7a440c94063ebf1ad1dd1b5646c7c5902f1d01
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-03-23 23:30:48 +0630

    feat: add varaibles

--- 02_fundamental/variables/src/main.rs
+++ 02_fundamental/variables/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let two = 2;
+    let hello = "hello";
+    let j = 'j';
+    let my_half = 0.5;
+    let mut my_name = "Aung Myat Moe";
+    let quite_program = false;
+    let your_half = my_half;
 }
