802d745c1ecec62caa853fe9e8471faa8a002de0
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2023-04-16 22:14:18 +0630

    resolve: map combinator

--- 11_easier_data_management/basic_closure/src/main.rs
+++ 11_easier_data_management/basic_closure/src/main.rs
@@ -2,4 +2,10 @@ fn main() {
     let add = |a, b| a + b;
     let result = add(1, 2);
     println!("{:?}", result);
+
+    let phoneRegx = Regex::new(r"(\d{3})-(\d{3,8})").unwrap();
+    let emailRegx = Regex::new(r"(\w+)@(\w+)\.(\w+)").unwrap();
 }
+
+
+
